06605c55e009d86b09f2fdf49c18128b9d301384
nmowens95 <nmowens95@gmail.com>
2023-12-08 15:34:08 -0600

    added basic test to benchmark time to loop through files

---
+++ Concurrency/countLines_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"testing"
+)
+
+func BenchmarkCountLines(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		readFile("file1.txt")
+	}
+}
+
+func BenchmarkCountLines2(b *testing.B) {
+	for i := 0; i < b.N; i++ { // N specifies number of iterations
+		readFile("file2.txt")
+	}
+}
+
+// go run -test <function>
